cd7a484acea57bb0d803260c13542bf6faf5d7d1
Wei Zhang <iusenenow@gmail.com>
2020-11-14 21:49:15 +1100

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,21 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-emotion`,
+      options: {
+        // Accepts the following options, all of which are defined by `babel-plugin-emotion` plugin.
+        // The values for each key in this example are the defaults the plugin uses.
+        sourceMap: true,
+        autoLabel: process.env.NODE_ENV !== "production",
+        labelFormat: `[local]`,
+        cssPropOptimization: true,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
