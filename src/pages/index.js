55d4ba030b6baea821d684631206e8e1eff66ca9
Wei Zhang <iusenenow@gmail.com>
2020-11-16 19:23:57 +1100

    finish touch

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
-import { graphql } from "gatsby"
 import { css } from "@emotion/core"
+import { Link, graphql } from "gatsby"
 import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
 
@@ -8,17 +8,40 @@ export default function Home({ data }) {
   return (
     <Layout>
       <div>
-        <h1 css={css`display: inline-block; border-bottom: 1px solid`}>
+        <h1
+          css={css`
+            display: inline-block;
+            border-bottom: 1px solid;
+          `}
+        >
           Amazing Pandas Eating Things
         </h1>
         <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
-            <h3 css={css`margin-bottom: ${rhythm(1 / 4)}`}>
-              {node.frontmatter.title}{" "}
-              <span css={css`color: #bbb`}>— {node.frontmatter.date}</span>
-            </h3>
-            <p>{node.excerpt}</p>
+            <Link
+              to={node.fields.slug}
+              css={css`
+                text-decoration: none;
+                color: inherit;
+              `}
+            >
+              <h3
+                css={css`
+                  margin-bottom: ${rhythm(1 / 4)};
+                `}
+              >
+                {node.frontmatter.title}{" "}
+                <span
+                  css={css`
+                    color: #555;
+                  `}
+                >
+                  — {node.frontmatter.date}
+                </span>
+              </h3>
+              <p>{node.excerpt}</p>
+            </Link>
           </div>
         ))}
       </div>
@@ -36,6 +59,9 @@ export const query = graphql`
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
           }
           excerpt
         }
